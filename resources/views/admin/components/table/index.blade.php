7154815c7ffb6c03938408b69b229a2d9d361adf
Ashish Dhamala <ashish.dhamala2015@gmail.com>
2022-07-24 20:27:03 +0100

    remove unnecessary html

--- resources/views/admin/components/table/index.blade.php
+++ resources/views/admin/components/table/index.blade.php
@@ -3,25 +3,21 @@
 ])
 
 <div class="overflow-x-auto">
-    <div class="py-2 inline-block min-w-full">
-        <div class="overflow-x-auto">
-            <table class="min-w-full">
-                <thead class="border-b">
-                    <tr>
-                        @foreach ($headers as $header)
-                        <th
-                            scope="col"
-                            class="text-sm font-medium text-gray-900 px-6 py-4 text-left"
-                        >
-                            {{ $header }}
-                        </th>
-                        @endforeach
-                    </tr>
-                </thead>
-                <tbody>
-                    {{ $slot }}
-                </tbody>
-            </table>
-        </div>
-    </div>
+    <table class="min-w-full">
+        <thead class="border-b">
+            <tr>
+                @foreach ($headers as $header)
+                <th
+                    scope="col"
+                    class="text-sm font-medium text-gray-900 px-4 py-2 text-left"
+                >
+                    {{ $header }}
+                </th>
+                @endforeach
+            </tr>
+        </thead>
+        <tbody>
+            {{ $slot }}
+        </tbody>
+    </table>
 </div>
